118eca90003624e664b0fe241add60e7a0123f89
nm0800-create <nm0800@srmist.edu.in>
2026-03-11 10:57:34 +0530

    Coded UC2: Display Hello with Command-Line Argument

---
+++ src/HelloApp.java
@@ -0,0 +1,8 @@
+public class HelloApp {
+    public static void main(String[] args) {
+
+        String name = (args.length > 0) ? args[0] : "Guest";
+        
+        System.out.println("Hello, " + name + "!");
+    }
+}
